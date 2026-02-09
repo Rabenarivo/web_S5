7629fe7b90bb38e92161623b641592bc72bb66bf
Nanto14 <andrianionynanto@gmail.com>
2026-02-09 15:21:45 +0300

    initialisation mobile

---
+++ compagnie_mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'compagnie_mobile_temp',
+  webDir: 'dist'
+};
+
+export default config;
